89b072a6262c8a3f7e86f87fb1b5c489fde425dd
sadfafdf <52762381+sadfafdf@users.noreply.github.com>
2019-07-11 00:27:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,30 +3,9 @@ const client = new Discord.Client()
 const prefix = "." // تقدر تغيره
 
 client.on('guildMemberAdd', member => {
-  
-  const channel = member.guild.channels.find(ch => ch.name === 'knights');
- 
-  if (!channel) return;
-
-  channel.send(`Welcome To Knights ${member}`);/// رسائل ترحيب
-});
- 
-const invites = {};
- 
-const wait = require('util').promisify(setTimeout);
- 
-client.on('ready', () => {
-  wait(1100);
- 
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-  });
-});
-
-
-
-
+    member.createDM().then(function (channel) {
+return channel.send("****Welcome to Knights. Please Join in Knights Shop :** :gear: https://discord.gg/EJate6v**")
+    }
+    )});
 
 client.login(process.env.TOKEN)
